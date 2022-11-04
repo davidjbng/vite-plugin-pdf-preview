6ec803e720906b8bf854bc1877e00803905272c1
David Jebing <29679909+davidjbng@users.noreply.github.com>
2022-11-04 14:35:52 +0100

    configure build

--- src/generatePdf.ts
+++ src/generatePdf.ts
@@ -1,30 +1,36 @@
-import { existsSync } from "fs"
-import { mkdir } from "fs/promises"
-import path from "path"
-import ppt from "puppeteer"
+import { existsSync } from "fs";
+import { mkdir } from "fs/promises";
+import path from "path";
+import ppt from "puppeteer";
 
-// start puppeteer on module load to speed up pdf generation
-const browser = await ppt.launch({ headless: true })
+let browser: ppt.Browser | undefined;
+async function initBrowser() {
+  if (!browser) {
+    browser = await ppt.launch({ headless: true });
+  }
+  return browser;
+}
 
 export type GeneratePdfOptions = {
-  pdfPath?: string
-  waitUntil?: ppt.WaitForOptions["waitUntil"]
-}
+  pdfPath?: string;
+  waitUntil?: ppt.WaitForOptions["waitUntil"];
+};
 
 /** This generates a pdf file from the given url */
 export async function generatePdf(
   URL: string,
   { pdfPath = "dist/preview.pdf", waitUntil = "load" }: GeneratePdfOptions = {}
 ) {
-  const outputDir = path.dirname(pdfPath)
+  const outputDir = path.dirname(pdfPath);
   if (!existsSync(outputDir)) {
-    await mkdir(outputDir, { recursive: true })
+    await mkdir(outputDir, { recursive: true });
   }
-  const page = await browser.newPage()
-  await page.goto(URL, { waitUntil })
+  const browser = await initBrowser();
+  const page = await browser.newPage();
+  await page.goto(URL, { waitUntil });
   await page.pdf({
     format: "A4",
     path: pdfPath,
     margin: { top: "16px", bottom: "16px" },
-  })
+  });
 }
